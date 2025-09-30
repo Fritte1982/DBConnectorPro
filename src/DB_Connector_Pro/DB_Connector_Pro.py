65a8ec16951aeaab82551362805afafebde60423
Fritte1982 <richard.bieligk@gmail.com>
2025-09-30 09:03:56 +0200

    Test import from a other structur-level

--- src/DB_Connector_Pro/DB_Connector_Pro.py
+++ src/DB_Connector_Pro/DB_Connector_Pro.py
@@ -1,1 +1,24 @@
 """Main module."""
+"""Test Import of DB_Connection"""
+from DBConnectorPro.DBConnectorPro import DB_Connection
+import os
+
+user: str = "emp_exercise_1"
+view_schema_keys = """SELECT *
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE """
+order_by_schema = "ORDER BY TABLE_NAME "
+schema_tabelle_name = view_schema_keys + order_by_schema
+user_file = r"user_file.json"
+
+
+def main() -> None:
+    print(os.getcwd())
+    db_excercis = DB_Connection(file_path=user_file)
+    db_excercis.set_user(user_key=user)
+    db_excercis.driver = "{ODBC Driver 17 for SQL Server}"
+    db_excercis.daten_spalten(view_schema_keys)
+    db_excercis.tabellen_ausgabe()
+
+
+if __name__ == "__main__":
+    main()
